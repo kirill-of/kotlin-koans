a64c7a6d502f23090aeb344f15d64d6bc89bc42a
Kirill_Ofitserov@epam.com <Rhfcjnf44>
2019-08-30 20:45:25 +0400

    added task desc

--- src/main/kotlin/pro/ofitserov/stepik/_1_introduction/_0_Hello_World/Task.kt
+++ src/main/kotlin/pro/ofitserov/stepik/_1_introduction/_0_Hello_World/Task.kt
@@ -1,5 +1,11 @@
 package pro.ofitserov.stepik._1_introduction._0_Hello_World
 
+/*
+Simple Functions
+
+Take a look at function syntax and make the function start return the string "OK".
+*/
+
 fun start(): String {
     return "OK"
 }
