f3e8898c32bc8530eb13657b3dd528cc2b57989e
Kirill_Ofitserov@epam.com <Rhfcjnf44>
2019-08-30 21:15:51 +0400

    added task

---
+++ src/main/kotlin/pro/ofitserov/stepik/_1_introduction/_1_Java_To_Kotlin_Converter/JavaCode.java
@@ -0,0 +1,21 @@
+package pro.ofitserov.stepik._1_introduction._1_Java_To_Kotlin_Converter;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class JavaCode {
+    public String toJSON(Collection<Integer> collection) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Iterator<Integer> iterator = collection.iterator();
+        while (iterator.hasNext()) {
+            Integer element = iterator.next();
+            sb.append(element);
+            if (iterator.hasNext()) {
+                sb.append(", ");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
