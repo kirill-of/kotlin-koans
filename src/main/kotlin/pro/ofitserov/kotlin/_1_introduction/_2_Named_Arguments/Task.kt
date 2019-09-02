175d9bfe7a473cd34441d93205068da1ac1186cf
Kirill_Ofitserov@epam.com <Rhfcjnf44>
2019-09-02 23:23:30 +0400

    added tasks

---
+++ src/main/kotlin/pro/ofitserov/kotlin/_1_introduction/_2_Named_Arguments/Task.kt
@@ -0,0 +1,24 @@
+package pro.ofitserov.kotlin._1_introduction._2_Named_Arguments
+
+import java.util.*
+
+/*
+Named arguments
+
+Default and named arguments help to minimize the number of overloads and improve the readability of the function invocation.
+The library function joinToString is declared with default values for parameters:
+fun joinToString(
+    separator: String = ", ",
+    prefix: String = "",
+    postfix: String = "",
+    /* ... */
+): String
+It can be called on a collection of Strings. Specifying only two arguments make the function joinOptions() return the list in a JSON format (e.g., "[a, b, c]")
+ */
+
+fun joinOptions(options: Collection<String>) = options.joinToString(prefix = "[", postfix = "]")
+
+fun main(args: Array<String>) {
+    val list = listOf("1", "2", "3")
+    println(joinOptions(list))
+}
