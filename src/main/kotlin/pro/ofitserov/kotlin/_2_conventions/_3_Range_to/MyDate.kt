358e9cc0b03aa00f1436924c2ff86b245ce51279
Kirill_Ofitserov@epam.com <Rhfcjnf44>
2019-09-04 10:36:29 +0400

    added task

---
+++ src/main/kotlin/pro/ofitserov/kotlin/_2_conventions/_3_Range_to/MyDate.kt
@@ -0,0 +1,9 @@
+package pro.ofitserov.kotlin._2_conventions._3_Range_to
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate>{
+    override fun compareTo(other: MyDate): Int{
+        if(year != other.year) return year - other.year
+        if(month != other.month) return month - other.month
+        return dayOfMonth - other.dayOfMonth
+    }
+}
