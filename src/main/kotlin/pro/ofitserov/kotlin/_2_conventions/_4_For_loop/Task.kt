2621c047a073419fb1315d1c743db26638816f20
Kirill_Ofitserov@epam.com <Rhfcjnf44>
2019-09-17 23:57:15 +0400

    added task

--- src/main/kotlin/pro/ofitserov/kotlin/_2_conventions/_4_For_loop/Task.kt
+++ src/main/kotlin/pro/ofitserov/kotlin/_2_conventions/_4_For_loop/Task.kt
@@ -1,5 +1,13 @@
 package pro.ofitserov.kotlin._2_conventions._4_For_loop
 
+/*
+For loop
+
+Kotlin for loop iterates through anything that provides an iterator.
+Make the class DateRange implement Iterable<MyDate>, so that it could be iterated over.
+You can use the function MyDate.nextDay() defined in DateUtil.kt
+*/
+
 class DateRange(val start: MyDate, val end: MyDate) : Iterable<MyDate> {
     override fun iterator(): Iterator<MyDate> = DateIterator(this)
 }
