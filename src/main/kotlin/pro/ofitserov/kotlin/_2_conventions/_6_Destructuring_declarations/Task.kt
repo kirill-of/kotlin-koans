7fc5acc8039728cef43c2bc6a0a0f6c2322d849c
Kirill_Ofitserov@epam.com <Rhfcjnf44>
2019-10-10 00:59:21 +0400

    added task

--- src/main/kotlin/pro/ofitserov/kotlin/_2_conventions/_6_Destructuring_declarations/Task.kt
+++ src/main/kotlin/pro/ofitserov/kotlin/_2_conventions/_6_Destructuring_declarations/Task.kt
@@ -1,2 +1,17 @@
 package pro.ofitserov.kotlin._2_conventions._6_Destructuring_declarations
 
+/*
+Destructuring declarations
+
+Read about destructuring declarations and make the following code compile by adding one word.
+*/
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+
+fun isLeapDay(date: MyDate): Boolean {
+
+    val (year, month, dayOfMonth) = date
+
+    // 29 February of a leap year
+    return year % 4 == 0 && month == 2 && dayOfMonth == 29
+}
