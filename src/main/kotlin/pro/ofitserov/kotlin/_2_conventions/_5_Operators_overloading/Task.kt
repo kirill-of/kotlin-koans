183857140c9c986cdee69a5b22f12803f788f9f9
Kirill_Ofitserov@epam.com <Rhfcjnf44>
2019-10-10 00:49:52 +0400

    added task

--- src/main/kotlin/pro/ofitserov/kotlin/_2_conventions/_5_Operators_overloading/Task.kt
+++ src/main/kotlin/pro/ofitserov/kotlin/_2_conventions/_5_Operators_overloading/Task.kt
@@ -1,2 +1,37 @@
 package pro.ofitserov.kotlin._2_conventions._5_Operators_overloading
 
+
+/*
+Operators overloading
+
+Implement a kind of date arithmetic. Support adding years, weeks and days to a date. You could be able to write the code like this: date + YEAR * 2 + WEEK * 3 + DAY * 15.
+At first, add an extension function 'plus()' to MyDate, taking a TimeInterval as an argument. Use an utility function MyDate.addTimeIntervals() declared in DateUtil.kt
+Then, try to support adding several time intervals to a date. You may need an extra class.
+*/
+
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+
+enum class TimeInterval { DAY, WEEK, YEAR }
+
+operator fun MyDate.plus(timeInterval: TimeInterval) = addTimeIntervals(timeInterval, 1)
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+
+operator fun TimeInterval.times(number: Int) = RepeatedTimeInterval(this, number)
+
+operator fun MyDate.plus(timeIntervals: RepeatedTimeInterval) =
+    addTimeIntervals(timeIntervals.timeInterval, timeIntervals.number)
+
+
+fun task1(today: MyDate): MyDate {
+    return today + TimeInterval.YEAR + TimeInterval.WEEK
+}
+
+fun task2(today: MyDate): MyDate {
+    return today + TimeInterval.YEAR * 2 + TimeInterval.WEEK * 3 + TimeInterval.DAY * 5
+}
+
+fun main() {
+    println(task1(MyDate(2019, 10, 10)))
+    println(task2(MyDate(2019, 10, 10)))
+}
