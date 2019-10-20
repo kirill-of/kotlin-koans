0c19319c14340c069e2ff1f382557d197e6992b6
Kirill_Ofitserov@epam.com <Rhfcjnf44>
2019-10-20 23:14:41 +0400

    added task

--- src/main/kotlin/pro/ofitserov/kotlin/_3_Collections/_5_MaxMin/Task.kt
+++ src/main/kotlin/pro/ofitserov/kotlin/_3_Collections/_5_MaxMin/Task.kt
@@ -6,3 +6,9 @@ Implement Shop.getCustomerWithMaximumNumberOfOrders() and Customer.getMostExpens
 
 listOf(1, 42, 4).max() == 42
 listOf("a", "ab").minBy { it.length } == "a"*/
+
+// Return a customer whose order count is the highest among all customers
+fun Shop.getCustomerWithMaximumNumberOfOrders(): Customer? = customers.maxBy { it.orders.size }
+
+// Return the most expensive product which has been ordered
+fun Customer.getMostExpensiveOrderedProduct(): Product? = orders.flatMap { it.products }.maxBy { it.price }
