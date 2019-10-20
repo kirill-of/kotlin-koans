855b5519e2dc8f97081e5a46d1ac1007d88fd53f
Kirill_Ofitserov@epam.com <Rhfcjnf44>
2019-10-20 23:55:15 +0400

    added tasks

--- src/main/kotlin/pro/ofitserov/kotlin/_3_Collections/_6_Sort/Task.kt
+++ src/main/kotlin/pro/ofitserov/kotlin/_3_Collections/_6_Sort/Task.kt
@@ -1,2 +1,12 @@
 package pro.ofitserov.kotlin._3_Collections._6_Sort
 
+/*Sort
+
+Implement Shop.getCustomersSortedByNumberOfOrders() using sorted or sortedBy.
+
+listOf("bbb", "a", "cc").sorted() == listOf("a", "bbb", "cc")
+listOf("bbb", "a", "cc").sortedBy { it.length } == listOf("a", "cc", "bbb")*/
+
+// Return a list of customers, sorted by the ascending number of orders they made
+fun Shop.getCustomersSortedByNumberOfOrders(): List<Customer> = customers.sortedBy { it.orders.size }
+
