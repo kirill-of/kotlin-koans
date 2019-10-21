4280573fc8c93958b6cada3f30215fa58ea17c71
Kirill_Ofitserov@epam.com <Rhfcjnf44>
2019-10-21 21:49:08 +0400

    added tasks

--- src/main/kotlin/pro/ofitserov/kotlin/_3_Collections/_8_GroupBy/Task.kt
+++ src/main/kotlin/pro/ofitserov/kotlin/_3_Collections/_8_GroupBy/Task.kt
@@ -1,2 +1,11 @@
 package pro.ofitserov.kotlin._3_Collections._8_GroupBy
 
+/*Group By
+
+Implement Shop.groupCustomersByCity() using groupBy.
+
+val result = listOf("a", "b", "ba", "ccc", "ad").groupBy { it.length() }
+result == mapOf(1 to listOf("a", "b"), 2 to listOf("ba", "ad"), 3 to listOf("ccc"))*/
+
+// Return a map of the customers living in each city
+fun Shop.groupCustomersByCity(): Map<City, List<Customer>> = customers.groupBy { it.city }
